c014b7a56c052a49d6aa64b954ccf53f4d37eb94
TaroFIN <karta2223899@gmail.com>
2025-03-21 16:57:36 +0800

    Modify Procedure
    修改Insert Procedure但時間到

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Mercuryfire_Backend_Test
  
+## 進度1：建置API & SQL表格 & SQL Procedure
+## 進度2：備份資料庫bak檔
--- TSQL/MyOffice_ACPD.sql
+++ TSQL/MyOffice_ACPD.sql
@@ -11,15 +11,24 @@ BEGIN
     SELECT * FROM MyOffice_ACPD WHERE ACPD_SID = @id
 END
 
-CREATE PROCEDURE InsertMyOffice_ACPD
+-- Insert MyOffice_ACPD
+ALTER PROCEDURE InsertMyOffice_ACPD
     @json NVARCHAR(MAX)
 AS
 BEGIN
+    DECLARE @NewID CHAR(20);
+    DECLARE @LogGroupID UNIQUEIDENTIFIER = NEWID();
+    DECLARE @LogReturnValues NVARCHAR(MAX);
+	DECLARE @RETURN NVARCHAR(20);
+
+    -- 呼叫 NEWSID 儲存過程來生成新的 ACPD_SID
+    EXEC @NewID = NEWSID @TableName='MyOffice_ACPD' @ReturnSID=@RETURN;
+
+    -- 插入 MyOffice_ACPD 記錄
     INSERT INTO MyOffice_ACPD (ACPD_SID, ACPD_Cname, ACPD_Ename, ACPD_Sname, ACPD_Email, ACPD_Status, ACPD_Stop, ACPD_StopMemo, ACPD_LoginID, ACPD_LoginPWD, ACPD_Memo, ACPD_NowDateTime, ACPD_NowID, ACPD_UPDDateTime, ACPD_UPDID)
-    SELECT ACPD_SID, ACPD_Cname, ACPD_Ename, ACPD_Sname, ACPD_Email, ACPD_Status, ACPD_Stop, ACPD_StopMemo, ACPD_LoginID, ACPD_LoginPWD, ACPD_Memo, ACPD_NowDateTime, ACPD_NowID, ACPD_UPDDateTime, ACPD_UPDID
+    SELECT @NewID, ACPD_Cname, ACPD_Ename, ACPD_Sname, ACPD_Email, ACPD_Status, ACPD_Stop, ACPD_StopMemo, ACPD_LoginID, ACPD_LoginPWD, ACPD_Memo, ACPD_NowDateTime, ACPD_NowID, ACPD_UPDDateTime, ACPD_UPDID
     FROM OPENJSON(@json)
     WITH (
-        ACPD_SID CHAR(20),
         ACPD_Cname NVARCHAR(60),
         ACPD_Ename NVARCHAR(40),
         ACPD_Sname NVARCHAR(40),
@@ -34,8 +43,21 @@ BEGIN
         ACPD_NowID NVARCHAR(20),
         ACPD_UPDDateTime DATETIME,
         ACPD_UPDID NVARCHAR(20)
-    )
+    );
+
+    -- 呼叫 usp_AddLog 儲存過程來記錄執行過程
+    EXEC dbo.usp_AddLog
+        @_InBox_ReadID = 0,
+        @_InBox_SPNAME = 'InsertMyOffice_ACPD',
+        @_InBox_GroupID = @LogGroupID,
+        @_InBox_ExProgram = 'Insert',
+        @_InBox_ActionJSON = @json,
+        @_OutBox_ReturnValues = @LogReturnValues OUTPUT;
+
+    -- 返回執行結果
+    SELECT @LogReturnValues AS LogResult;
 END
+
 
 CREATE PROCEDURE UpdateMyOffice_ACPD
     @id CHAR(20),
